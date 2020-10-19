633d784c9edcc3a6d3a87c12efa6a431d8052d61
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-10-19 21:00:44 +0100

    Updates

---
+++ precache-manifest.922294a1f46a77f242f66c0c85765664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb1c16a7621683bacc487227dd54720",
+    "url": "/todo-list-app-with-react-hooks/index.html"
+  },
+  {
+    "revision": "4a2212996e97a2fed6d1",
+    "url": "/todo-list-app-with-react-hooks/static/css/main.d6588497.chunk.css"
+  },
+  {
+    "revision": "06d3f2b36750ac638fb5",
+    "url": "/todo-list-app-with-react-hooks/static/js/2.80e11e4f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list-app-with-react-hooks/static/js/2.80e11e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2212996e97a2fed6d1",
+    "url": "/todo-list-app-with-react-hooks/static/js/main.0c1df6e0.chunk.js"
+  },
+  {
+    "revision": "4cd54496ebc0fc28d7a2",
+    "url": "/todo-list-app-with-react-hooks/static/js/runtime-main.a358869f.js"
+  }
+]);
